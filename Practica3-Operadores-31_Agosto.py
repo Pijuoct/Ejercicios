cb6f163543eac914bc9a2aced34efaeff11accdc
Pijuoct <juanes2003oct17@gmail.com>
2022-08-31 14:42:51 -0500

    test commits

--- Practica3-Operadores-31_Agosto.py
+++ Practica3-Operadores-31_Agosto.py
@@ -1,4 +1,7 @@
 #Operador de asignacion: =
+from tkinter import E
+
+
 a=1
 b=2
 c=3
@@ -7,6 +10,21 @@ c=3
 print("suma sencilla:",1+2+3)
 print("multiplicacion con prioridad caso 1:",1*2+3)
 print("multiplicacion con prioridad caso 2:",1+2*3)
+print("division sencilla:",5/3)
+print("division entera:",5//3)
+print("residuo divison:",5%3)
+print("potenciacion:",9**2)
+print("raices (potencia decimal)",9**0.5)
+
+#P = parentesis
+#E = exponentes
+#M = multiplicacion
+#D = division
+#A = adicion
+#S = sustraccion
+
+# la multiplicaicon y division estan en iguales niveles de jerarquia
+# su prioridad se asigna de izquierda a derecha
 
 #Operadores logicos: not, and, or
 
