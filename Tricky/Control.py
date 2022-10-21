89e26b9e227263cb5bd072b0126a7d79c881260b
Pijuoct <juanes2003oct17@gmail.com>
2022-10-21 17:53:04 -0500

    Mejoras (Empates, verificacion tipo de dato ingresado)

--- Tricky/Control.py
+++ Tricky/Control.py
@@ -3,8 +3,10 @@ import Interfaz
 Interfaz.explicarJuego()
 
 
-jugador1=input('Ingrese que simbolo sera el 1er jugador ("X" o "O")--> ')
+jugador1=input('Ingrese que simbolo sera el 1er jugador ("X" o "O") --> ')
 
+while jugador1!='X' and jugador1!='O':
+    jugador1=input('Ingrese un simbolo VALIDO ("X" o "O") --> ')
 
 if jugador1=='X':
     jugador2='O'
@@ -20,7 +22,7 @@ print('¡EMPEZAMOS!')
 
 hayganador=Logica.determinarGandor(tableroLogico)
 turno=1
-while hayganador==None:
+while hayganador==None and None in tableroLogico:
     
     if (turno%2)!=0:
         print('Jugador 1 (%s) ingrese la posicion de su jugada:'%jugador1)
@@ -29,7 +31,12 @@ while hayganador==None:
         print('Jugador 2 (%s) ingrese la posicion de su jugada:'%jugador2)
         caracter=jugador2
 
-    pos=int(input('--> '))
+    pos=input('--> ')
+
+    while pos not in ('0','1','2','3','4','5','6','7','8'):
+        pos=input('Ingrese una posicion valida (0-8) --> ')
+    
+    pos=int(pos)
 
     tableroLogico=Logica.insertarCaracterEnTablero(tableroLogico,pos,caracter)
 
@@ -41,9 +48,12 @@ while hayganador==None:
 
 turno=turno-1
 
-if turno%2!=0:
-    ganador=1
+if None not in tableroLogico:
+    print('Empate, No hay ganador')
 else:
-    ganador=2
+    if turno%2!=0:
+        ganador=1
+    else:
+        ganador=2
 
-print('El ganador es el jugador %i con las %s'%(ganador,caracter))
+    print('El ganador es el jugador %i con las %s'%(ganador,caracter))
