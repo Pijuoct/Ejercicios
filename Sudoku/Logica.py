a142f461b76dbf1ca2c9c7dd6c94339425f801cd
Pijuoct <juanes2003oct17@gmail.com>
2022-10-21 20:45:13 -0500

    Logica - Insertar Numero en el Tablero

--- Sudoku/Logica.py
+++ Sudoku/Logica.py
@@ -4,7 +4,7 @@ Este modulo contiene 3 funciones:
 * generarTableroLogico(): No recibe nada,
                           retonra una lista con 9 listas de Nones
 
-* insertarCaracterEntablero(): Recibe (lista,posicion,caracter)
+* insertarNumeroEntablero(): Recibe (lista de listas,coordenadas,numero)
                                retorna una lista actualizada
 
 * determinarGanador(): Recibe una lista(tablero)
@@ -13,7 +13,55 @@ Este modulo contiene 3 funciones:
 """
 
 def generarTableroLogico():
-    tableroLogico=[[None]*9]*9
+    tableroLogico=[[None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None],
+                   [None,None,None,None,None,None,None,None,None]]
     
     return tableroLogico
+
+
+def insertarNumeroEnTablero(tableroLogico:list,coordenadas:str,numero:str):
+
+    [cuadro_mayor,posicion]=coordenadas.split('-')
+
+    cuadro_mayor=int(cuadro_mayor)
+    posicion=int(posicion)
+
+    while tableroLogico[cuadro_mayor][posicion]!=None:
+        
+        coordenadas=input('Ingrese unas coordenadas DISPONIBLE por favor --> ')
+        [cuadro_mayor,posicion]=coordenadas.split('-')
+
+        while (cuadro_mayor and posicion) not in ('0','1','2','3','4','5','6','7','8'):
+            coordenadas=input('Ingrese una coordenadas VALIDA y DISPONIBLE (0-8) --> ')
+            [cuadro_mayor,posicion]=coordenadas.split('-')
+        
+        cuadro_mayor=int(cuadro_mayor)
+        posicion=int(posicion)
+
+    tableroLogico[cuadro_mayor][posicion]=numero
+
+    return tableroLogico
+
+
+if __name__=="__main__":
+    import Interfaz
+
+    tablero=generarTableroLogico()
+    Interfaz.imprimirTablero(tablero)
+
+
+    tablero=insertarNumeroEnTablero(tablero,'4-8','8')
+    tablero=insertarNumeroEnTablero(tablero,'2-6','9')
+    tablero=insertarNumeroEnTablero(tablero,'5-1','2')
+    tablero=insertarNumeroEnTablero(tablero,'0-3','9')
+    tablero=insertarNumeroEnTablero(tablero,'7-8','1')
+
+    Interfaz.imprimirTablero(tablero)
 
