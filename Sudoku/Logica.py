16bf4c00e976a385ea1968237f675468ebfbfc9a
Pijuoct <juanes2003oct17@gmail.com>
2022-10-22 01:12:37 -0500

    logica - NO repetir lineas

--- Sudoku/Logica.py
+++ Sudoku/Logica.py
@@ -56,6 +56,55 @@ def generarNumerosFijos(tableroLogico_1:list,tableroLogico_2):
 
     cuadros=(0,1,2,3,4,5,6,7,8)
 
+    lineasH=[((0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)),
+             ((0,3),(0,4),(0,5),(1,3),(1,4),(1,5),(2,3),(2,4),(2,5)),
+             ((0,6),(0,7),(0,8),(1,6),(1,7),(1,8),(2,6),(2,7),(2,8)),
+             ((3,0),(3,1),(3,2),(4,0),(4,1),(4,2),(5,0),(5,1),(5,2)),
+             ((3,3),(3,4),(3,5),(4,3),(4,4),(4,5),(5,3),(5,4),(5,5)),
+             ((3,6),(3,7),(3,8),(4,6),(4,7),(4,8),(5,6),(5,7),(5,8)),
+             ((6,0),(6,1),(6,2),(7,0),(7,1),(7,2),(8,0),(8,1),(8,2)),
+             ((6,3),(6,4),(6,5),(7,3),(7,4),(7,5),(8,3),(8,4),(8,5)),
+             ((6,6),(6,7),(6,8),(7,6),(7,7),(7,8),(8,6),(8,7),(8,8))]
+
+    lineasV=[((0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)),
+             ((1,0),(1,3),(1,6),(4,0),(4,3),(4,6),(7,0),(7,3),(7,6)),
+             ((2,0),(2,3),(2,6),(5,0),(5,3),(5,6),(8,0),(8,3),(8,6)),
+             ((0,1),(0,4),(0,7),(3,1),(3,4),(3,7),(6,1),(6,4),(6,7)),
+             ((1,1),(1,4),(1,7),(4,1),(4,4),(4,7),(7,1),(7,4),(7,7)),
+             ((2,1),(2,4),(2,7),(5,1),(5,4),(5,7),(8,1),(8,4),(8,7)),
+             ((0,2),(0,5),(0,8),(3,2),(3,5),(3,8),(6,2),(6,5),(6,8)),
+             ((1,2),(1,5),(1,8),(4,2),(4,5),(4,8),(7,2),(7,5),(7,8)),
+             ((2,2),(2,5),(2,8),(5,2),(5,5),(5,8),(8,2),(8,5),(8,8))]
+
+    NombresH=['H1','H2','H3','H4','H5','H6','H7','H8','H9']
+
+    NombresV=['V1','V4','V7','V2','V5','V8','V3','V6','V9']
+
+    lineasHDic=dict(zip(NombresH,lineasH))
+
+    lineasVDic=dict(zip(NombresV,lineasV))
+
+    duplas=[]
+    lineasInterceptasLista=[]
+
+    for cuadro in cuadros:
+        for posicion in range(0,9):
+            dupla=(cuadro,posicion)
+            duplas.append(str(dupla).replace(' ',''))
+
+            listaIndex=[]
+            for index in lineasHDic.keys():
+                if dupla in lineasHDic[index]:
+                    listaIndex.append(index)
+
+            for index in lineasVDic.keys():
+                if dupla in lineasVDic[index]:
+                    listaIndex.append(index)
+
+            lineasInterceptasLista.append(listaIndex)
+
+    lineasInterceptasDic=dict(zip(duplas,lineasInterceptasLista))
+
     for cuadro in cuadros:
 
         posicion=[0,1,2,3,4,5,6,7,8]
@@ -69,8 +118,24 @@ def generarNumerosFijos(tableroLogico_1:list,tableroLogico_2):
             pos=random.choice(posicion)
             posicion.remove(pos)
 
+            [linea1,linea2]=lineasInterceptasDic['('+str(cuadro)+','+str(pos)+')']
+
+            fila1=[]
+            fila2=[]
+
+            for cuadro1,posicion1 in lineasHDic[linea1]:
+                numeroExistente=tableroLogico_1[cuadro1][posicion1]
+                fila1.append(numeroExistente)
+
+            for cuadro1,posicion1 in lineasVDic[linea2]:
+                numeroExistente=tableroLogico_1[cuadro1][posicion1]
+                fila2.append(numeroExistente)
+            
+
             num=random.choice(numeros)
-            numeros.remove(num)
+            while num in fila1 or num in fila2:
+                numeros.remove(num)
+                num=random.choice(numeros)
 
             tableroLogico_1[cuadro][pos]=num
             tableroLogico_2[cuadro][pos]=num
@@ -86,8 +151,6 @@ if __name__=="__main__":
     [tablero,tableroFijo]=generarNumerosFijos(tablero,tableroFijo)
     
 
-    tablero=insertarNumeroEnTablero(tablero,'0-0','8')
-    tablero=insertarNumeroEnTablero(tablero,'1-0','8')
 
 
     Interfaz.imprimirTablero(tableroFijo)
